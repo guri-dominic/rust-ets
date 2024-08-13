3322eb5d6458c8be0a85fb707052176906d1791b
Dominic Guri <dominic.guri@gmail.com>
2024-08-13 14:33:14 -0400

    more basic features for ETS (sequence), ET transformations, and SE3 using nalgebra

---
+++ src/lib.rs
@@ -0,0 +1,186 @@
+extern crate nalgebra as na;
+use na::base::Matrix4;
+use na::{Rotation3, Vector3};
+use std::ops::Mul;
+
+#[derive(Clone)]
+pub struct SE3 {
+    data: Matrix4<f32>,
+}
+
+impl std::fmt::Display for SE3 {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", self.data)
+    }
+}
+
+impl SE3 {
+    pub fn new() -> SE3 {
+        SE3 {
+            data: Matrix4::<f32>::identity(),
+        }
+    }
+    pub fn rpy(roll: f32, pitch: f32, yaw: f32) -> SE3 {
+        let mut transform = Matrix4::<f32>::identity();
+        let rot = Rotation3::from_euler_angles(roll, pitch, yaw);
+        transform
+            .fixed_view_mut::<3, 3>(0, 0)
+            .copy_from(&rot.matrix());
+        SE3 { data: transform }
+    }
+    pub fn rx(angle: f32) -> SE3 {
+        let mut transform = Matrix4::<f32>::identity();
+        let rot = Rotation3::from_euler_angles(angle, 0.0, 0.0);
+        transform
+            .fixed_view_mut::<3, 3>(0, 0)
+            .copy_from(&rot.matrix());
+        SE3 { data: transform }
+    }
+    pub fn ry(angle: f32) -> SE3 {
+        let mut transform = Matrix4::<f32>::identity();
+        let rot = Rotation3::from_euler_angles(0.0, angle, 0.0);
+        transform
+            .fixed_view_mut::<3, 3>(0, 0)
+            .copy_from(&rot.matrix());
+        SE3 { data: transform }
+    }
+    pub fn rz(angle: f32) -> SE3 {
+        let mut transform = Matrix4::<f32>::identity();
+        let rot = Rotation3::from_euler_angles(0.0, 0.0, angle);
+        transform
+            .fixed_view_mut::<3, 3>(0, 0)
+            .copy_from(&rot.matrix());
+        SE3 { data: transform }
+    }
+    pub fn tx(d: f32) -> SE3 {
+        let mut transform = Matrix4::<f32>::identity();
+        transform[(0, 3)] = d;
+        SE3 { data: transform }
+    }
+    pub fn ty(d: f32) -> SE3 {
+        let mut transform = Matrix4::<f32>::identity();
+        transform[(1, 3)] = d;
+        SE3 { data: transform }
+    }
+    pub fn tz(d: f32) -> SE3 {
+        let mut transform = Matrix4::<f32>::identity();
+        transform[(2, 3)] = d;
+        SE3 { data: transform }
+    }
+
+    pub fn matrix(self) -> Matrix4<f32> {
+        self.data.clone()
+    }
+}
+
+// impl<T> std::ops::Mul<T> for SE3 {
+//     fn mul(self, rhs: SE3) -> SE3 {
+//         SE3 {
+//             data: rhs.matrix() * self.matrix(),
+//         }
+//     }
+// }
+
+impl<RHS: Copy> Mul<RHS> for SE3
+where
+    SE3: Mul<RHS, Output = SE3>, RHS
+{
+    type Output = SE3;
+
+    fn mul(self, rhs: RHS) -> SE3 {
+        SE3 {
+            // data: <Mul<RHS>>::mul(self.matrix(), rhs.matrix()),
+            data: self.data * rhs.matrix(),
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
+pub enum ET {
+    I,
+    Tx {
+        d: f32,
+    },
+    Ty {
+        d: f32,
+    },
+    Tz {
+        d: f32,
+    },
+    Rx {
+        angle: f32,
+    },
+    Ry {
+        angle: f32,
+    },
+    Rz {
+        angle: f32,
+        isjoint: bool, // TODO: use Maybe<usize> for joint or not
+        index: i32,
+    },
+}
+
+impl ET {
+    pub fn new() -> ET {
+        ET::I
+    }
+    pub fn tx(x: f32) -> ET {
+        ET::Tx { d: x }
+    }
+    pub fn ty(y: f32) -> ET {
+        ET::Ty { d: y }
+    }
+    pub fn tz(z: f32) -> ET {
+        ET::Tz { d: z }
+    }
+    pub fn rx(roll: f32) -> ET {
+        ET::Rx { angle: roll }
+    }
+    pub fn ry(pitch: f32) -> ET {
+        ET::Ry { angle: pitch }
+    }
+    pub fn rz(yaw: f32) -> ET {
+        ET::Rz {
+            angle: yaw,
+            isjoint: false,
+            index: -1,
+        }
+    }
+    pub fn eval(&self) -> SE3 {
+        match *self {
+            ET::I => SE3::new(),
+            ET::Tx { d } => return SE3::tx(d),
+            ET::Ty { d } => return SE3::ty(d),
+            ET::Tz { d } => return SE3::tz(d),
+            ET::Rx { angle } => return SE3::rx(angle),
+            ET::Ry { angle } => return SE3::ry(angle),
+            // TODO: if angle
+            ET::Rz {
+                angle,
+                isjoint,
+                index,
+            } => {
+                let t = SE3::rz(angle);
+                return t;
+            }
+        }
+    }
+}
+
+impl std::fmt::Display for ET {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match *self {
+            ET::I => write!(f, "SE3()"),
+            ET::Tx { d } => write!(f, "Tx({d})"),
+            ET::Ty { d } => write!(f, "Ty({d})"),
+            ET::Tz { d } => write!(f, "Tz({d})"),
+            ET::Rx { angle } => write!(f, "Rx({angle})"),
+            ET::Ry { angle } => write!(f, "Ry({angle})"),
+            ET::Rz {
+                angle,
+                isjoint,
+                index,
+            } => write!(f, "Rz({angle},isjoint={isjoint})"),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,110 +3,64 @@ extern crate nalgebra as na;
 use na::base::Matrix4;
 use na::{Rotation3, Vector3};
 
-struct SE3 {
-    data: Matrix4<f32>,
-}
+use ets::*;
 
-impl SE3 {
-    fn new() -> SE3 {
-        SE3 {
-            data: Matrix4::<f32>::identity(),
-        }
-    }
-    fn rpy(roll: f32, pitch: f32, yaw: f32) -> SE3 {
-        let mut transform = Matrix4::<f32>::identity();
-        let rot = Rotation3::from_euler_angles(roll, pitch, yaw);
-        transform
-            .fixed_view_mut::<3, 3>(0, 0)
-            .copy_from(&rot.matrix());
-        SE3 { data: transform }
-    }
-    fn rx(angle: f32) -> SE3 {
-        let mut transform = Matrix4::<f32>::identity();
-        let rot = Rotation3::from_euler_angles(angle, 0.0, 0.0);
-        transform
-            .fixed_view_mut::<3, 3>(0, 0)
-            .copy_from(&rot.matrix());
-        SE3 { data: transform }
-    }
-    fn ry(angle: f32) -> SE3 {
-        let mut transform = Matrix4::<f32>::identity();
-        let rot = Rotation3::from_euler_angles(0.0, angle, 0.0);
-        transform
-            .fixed_view_mut::<3, 3>(0, 0)
-            .copy_from(&rot.matrix());
-        SE3 { data: transform }
-    }
-    fn rz(angle: f32) -> SE3 {
-        let mut transform = Matrix4::<f32>::identity();
-        let rot = Rotation3::from_euler_angles(0.0, 0.0, angle);
-        transform
-            .fixed_view_mut::<3, 3>(0, 0)
-            .copy_from(&rot.matrix());
-        SE3 { data: transform }
-    }
-    fn tx(d: f32) -> SE3 {
-        let mut transform = Matrix4::<f32>::identity();
-        transform[(0, 3)] = d;
-        SE3 { data: transform }
-    }
-    fn ty(d: f32) -> SE3 {
-        let mut transform = Matrix4::<f32>::identity();
-        transform[(1, 3)] = d;
-        SE3 { data: transform }
-    }
-    fn tz(d: f32) -> SE3 {
-        let mut transform = Matrix4::<f32>::identity();
-        transform[(2, 3)] = d;
-        SE3 { data: transform }
-    }
-}
-
-enum ET {
-    I,
-    Tx {
-        d: f32,
-    },
-    Ty {
-        d: f32,
-    },
-    Tz {
-        d: f32,
-    },
-    Rx {
-        angle: f32,
-    },
-    Ry {
-        angle: f32,
-    },
-    Rz {
-        angle: f32,
-        isjoint: bool,
-        index: i32,
-    },
-}
-
+#[derive(Clone)]
 struct ETS {
-    ets:Vec<ET>
-    // add more parameters
-    // n -> number of dofs
-    // joints -> indices of all joint transformations
-    // ...
+    // mutable?
+    ets: Vec<ET>, // add more parameters
+                  // n -> number of dofs
+                  // joints -> indices of all joint transformations
+                  // ...
 }
 
 impl ETS {
-    fn eval(q: Vec<f32>, base: SE3, tool: SE3) -> SE3 {
-        SE3::new()
+    fn eval(&self, q: Vec<f32>, base: SE3, tool: SE3) -> SE3 {
+        let mut t = SE3::new();
+        for e in self.ets.iter() {
+            // chain.ets.push(e.clone())
+            t = t * e.eval();
+        }
+        t
     }
-    fn new(et: ET) -> ETS {
-        ETS { ets : vec![et] }
+    fn new() -> ETS {
+        ETS {
+            ets: Vec::<ET>::new(),
+        }
+    }
+    fn from_vec(et: Vec<ET>) -> ETS {
+        let mut chain = ETS::new();
+        for e in et.iter() {
+            chain.ets.push(e.clone())
+        }
+        chain
+    }
+}
+
+impl std::fmt::Display for ETS {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        if self.ets.len() == 0 as usize {
+            write!(f, "{}", ET::I)?;
+            return Ok(());
+        }
+
+        for k in 0..(self.ets.len()) {
+            let v = self.ets[k];
+            write!(f, "{}", v)?;
+            if k < (self.ets.len() - 1) {
+                // write!(f, " ⋅ ")?;
+                write!(f, "⋅")?;
+                // write!(f, " ⊗ ")?;
+            }
+        }
+        Ok(())
     }
 }
 
 fn main() {
     // let t = Matrix4::<f32>::identity();
     let t = SE3::new();
-    println!("t = {}", t.data);
+    println!("t = {}", t);
 
     // let axisangle = Vector3::y() * std::f32::consts::FRAC_PI_2;
     // let axisangle = Vector3::y() * 0.0f32;
@@ -114,12 +68,32 @@ fn main() {
     // println!("t = {}", rot);
 
     let r = SE3::rx(std::f32::consts::FRAC_PI_2);
-    println!("r = {}", r.data);
+    println!("r = {}", r);
 
     // let t = SE3::tx(1.2);
-    // println!("t = {}", t.data);
+    // println!("t = {}", t);
     // let t = SE3::ty(1.2);
-    // println!("t = {}", t.data);
+    // println!("t = {}", t);
     // let t = SE3::tz(1.2);
-    // println!("t = {}", t.data);
+    // println!("t = {}", t);
+
+    let etz = vec![
+        ET::I,
+        ET::tx(1.0),
+        ET::ty(1.0),
+        ET::tz(1.0),
+        ET::rx(1.0),
+        ET::ry(1.0),
+        ET::rz(1.0),
+    ];
+
+    for et in etz.iter() {
+        println!("et = {}", et);
+    }
+
+    let etz1 = ETS::new();
+    println!("etz1 = {}", etz1);
+
+    let etz1 = ETS::from_vec(etz);
+    println!("etz1 = {}", etz1);
 }
